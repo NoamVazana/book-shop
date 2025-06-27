d8d14f482595187b03eb1bb4b1b52785ffc47c45
NoamVazana <noamvaz@gmail.com>
2025-06-27 23:36:29 +0300

    Add and implement remove book function + connect functionality to delete button + add id to books with a function in a util js file.

--- js/book.controller.js
+++ js/book.controller.js
@@ -5,19 +5,30 @@ function onInit(){
 }
 
 function render(){
+    const thHtml = `
+    <tr>
+      <th>Title</th>
+      <th>Price</th>
+      <th>Actions</th>
+    </tr>`
+
     const elBookTable = document.querySelector('.book-table')
     const Books = getBooks()
 
-    var srtHtml = Books.map(book => `
+    var strHtml = Books.map(book => `
         <tr>
             <td>${book.name}</td>
             <td>${book.price}</td>
             <td>
-                <button class="read">Read</button>
-                <button class="update">Update</button>
-                <button class="delete">Delete</button>            </td>
+                <button class="read" onClick='onReadBook("${book.id}")'>Read</button>
+                <button class="update" onClick='onUpdateBook("${book.id}")'>Update</button>
+                <button class="delete" onClick='onRemoveBook("${book.id}")'>Delete</button>            </td>
         </tr>
         `)
-    elBookTable.innerHTML += srtHtml.join('')
+    elBookTable.innerHTML = thHtml + strHtml.join('')
+}
 
+function onRemoveBook(id){
+    removeBook(id)
+    render()
 }
--- js/book.service.js
+++ js/book.service.js
@@ -1,11 +1,16 @@
 'use strict'
 
 const gBooks = [
-    {name:'Adventure time', price:120 },
-    {name:"Jojo's bizzare adventure", price:120 },
-    {name:'Mob psycho 100', price:120 }
+    {id: makeid(), name:'Adventure time', price:120 },
+    {id: makeid(), name:"Jojo's bizzare adventure", price:120 },
+    {id: makeid(), name:'Mob psycho 100', price:120 }
 ]
 
 function getBooks(){
     return gBooks
+}
+
+function removeBook(id){
+    const bookIdx = gBooks.findIndex(book => book.id === id)
+    gBooks.splice(bookIdx, 1)
 }
