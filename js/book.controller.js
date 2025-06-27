43081125582f29b56c19c262c469f0075c340ca8
NoamVazana <noamvaz@gmail.com>
2025-06-27 23:01:20 +0300

    Implemet the render() function

--- js/book.controller.js
+++ js/book.controller.js
@@ -5,5 +5,19 @@ function onInit(){
 }
 
 function render(){
-    
+    const elBookTable = document.querySelector('.book-table')
+    const Books = getBooks()
+
+    var srtHtml = Books.map(book => `
+        <tr>
+            <td>${book.name}</td>
+            <td>${book.price}</td>
+            <td>
+                <button class="read">Read</button>
+                <button class="update">Update</button>
+                <button class="delete">Delete</button>            </td>
+        </tr>
+        `)
+    elBookTable.innerHTML += srtHtml.join('')
+
 }
