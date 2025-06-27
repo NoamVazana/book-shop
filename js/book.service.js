aa8132713110568baf4c3dceb678b398b923ee80
NoamVazana <noamvaz@gmail.com>
2025-06-27 22:34:38 +0300

    Add books modal in a service js file and a basic init function in a controller js file

---
+++ js/book.service.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const gBooks = [
+    {name:'Adventure time', price:120 },
+    {name:"Jojo's bizzare adventure", price:120 },
+    {name:'Mob psycho 100', price:120 }
+]
+
+function getBooks(){
+    return gBooks
+}
