7e880e6d377c055747c5f3ab7ba7c3fef84d6722
NoamVazana <noamvaz@gmail.com>
2025-06-28 01:29:14 +0300

    implement function to create the initial books model and add info and image to book objects

--- js/book.service.js
+++ js/book.service.js
@@ -1,10 +1,6 @@
 'use strict'
 
-const gBooks = [
-    {id: makeid(), name:'Adventure time', price:120 },
-    {id: makeid(), name:"Jojo's bizzare adventure", price:120 },
-    {id: makeid(), name:'Mob psycho 100', price:120 }
-]
+const gBooks = createInitBooks()
 
 function getBooks(){
     return gBooks
@@ -24,8 +20,24 @@ function addBook(name, price){
     gBooks.push(createBook(name, price))
 }
 
-function createBook(name, price){
-    return {id: makeid(), name, price}
+function createInitBooks(){
+    const atImgUrl = 'https://m.media-amazon.com/images/M/MV5BMjkxMzIwNmQtMzM5Ni00YWJiLTg4YjQtNjBiN2IxMjZhMGQ2XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg'
+    const atInfo = 'A psychic middle school boy tries to live a normal life and keep his growing powers under control, even though he constantly gets into trouble.'
+    const jojosImgUrl ='https://m.media-amazon.com/images/M/MV5BMzIyNzY4NTMtNmVhYS00OWFhLTkwMWMtOGFkNTdmNWU2ZDdiXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg'
+    const jojosInfo = 'The story of the Joestar family, who are possessed with intense psychic strength, and the adventures each member encounters throughout their lives.'
+    const MobPsyImgUrl = 'https://m.media-amazon.com/images/M/MV5BYzU3NDM4ZjgtY2UyMi00YTczLTgyNDEtMjBiMDJlOGUxNjcxXkEyXkFqcGc@._V1_.jpg'
+    const mobPsyInfo = 'A 12-year-old boy and his best friend, a wise 28-year-old dog with magical powers, go on a series of surreal adventures in a remote future.'
+    return [
+        createBook('Adventure time', 120, atImgUrl,),
+        createBook("Jojo's bizzare adventure", 120,jojosImgUrl, ),
+        createBook('Mob psycho 100', 120, MobPsyImgUrl, )
+    ]
+}
+
+function createBook(name, price, coverImgUrl, info){
+    if(!coverImgUrl) coverImgUrl = 'https://png.pngtree.com/png-vector/20221125/ourmid/pngtree-no-image-available-icon-flatvector-illustration-pic-design-profile-vector-png-image_40966566.jpg'
+    if(!info) info = 'No available info'
+    return {id: makeid(), name, price, coverImgUrl, info}
 }
 
 function getBookById(id){
